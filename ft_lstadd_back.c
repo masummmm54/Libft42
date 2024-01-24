84c1c4d6e91faee60c66d1af15080df739f67bd1
Muhammet Kose <muhakose@2-A-4.42heilbronn.de>
2024-01-24 10:38:07 +0100

    asd

---
+++ ft_lstadd_back.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhakose <muhakose@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 14:46:31 by muhakose          #+#    #+#             */
+/*   Updated: 2023/10/16 09:54:09 by muhakose         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (lst)
+	{
+		if (*lst)
+		{
+			last = ft_lstlast(*lst);
+			last -> next = new;
+		}
+		else
+			*lst = new;
+	}
+}
